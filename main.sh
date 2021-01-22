97d1dac96a674b03e1413bee0f008157b2346132
shivamsoin <68854577+shivamsoin@users.noreply.github.com>
2021-01-22 23:00:01 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,108 @@
+aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-xxxxxxxx --subnet-id subnet-xxxxxxxx
+{
+    "OwnerId": "123456789012",
+    "ReservationId": "r-5875ca20",
+    "Groups": [
+        {
+            "GroupName": "my-sg",
+            "GroupId": "sg-903004f8"
+        }
+    ],
+    "Instances": [
+        {
+            "Monitoring": {
+                "State": "disabled"
+            },
+            "PublicDnsName": null,
+            "Platform": "windows",
+            "State": {
+                "Code": 0,
+                "Name": "pending"
+            },
+            "EbsOptimized": false,
+            "LaunchTime": "2013-07-19T02:42:39.000Z",
+            "PrivateIpAddress": "10.0.1.114",
+            "ProductCodes": [],
+            "VpcId": "vpc-1a2b3c4d",
+            "InstanceId": "i-5203422c",
+            "ImageId": "ami-173d747e",
+            "PrivateDnsName": ip-10-0-1-114.ec2.internal,
+            "KeyName": "MyKeyPair",
+            "SecurityGroups": [
+                {
+                    "GroupName": "my-sg",
+                    "GroupId": "sg-903004f8"
+                }
+            ],
+            "ClientToken": null,
+            "SubnetId": "subnet-6e7f829e",
+            "InstanceType": "t2.micro",
+            "NetworkInterfaces": [
+                {
+                    "Status": "in-use",
+                    "SourceDestCheck": true,
+                    "VpcId": "vpc-1a2b3c4d",
+                    "Description": "Primary network interface",
+                    "NetworkInterfaceId": "eni-a7edb1c9",
+                    "PrivateIpAddresses": [
+                        {
+                            "PrivateDnsName": "ip-10-0-1-114.ec2.internal",
+                            "Primary": true,
+                            "PrivateIpAddress": "10.0.1.114"
+                        }
+                    ],
+                    "PrivateDnsName": "ip-10-0-1-114.ec2.internal",
+                    "Attachment": {
+                        "Status": "attached",
+                        "DeviceIndex": 0,
+                        "DeleteOnTermination": true,
+                        "AttachmentId": "eni-attach-52193138",
+                        "AttachTime": "2013-07-19T02:42:39.000Z"
+                    },
+                    "Groups": [
+                        {
+                            "GroupName": "my-sg",
+                            "GroupId": "sg-903004f8"
+                        }
+                    ],
+                    "SubnetId": "subnet-6e7f829e",
+                    "OwnerId": "123456789012",
+                    "PrivateIpAddress": "10.0.1.114"
+                }              
+            ],
+            "SourceDestCheck": true,
+            "Placement": {
+                "Tenancy": "default",
+                "GroupName": null,
+                "AvailabilityZone": "us-west-2b"
+            },
+            "Hypervisor": "xen",
+            "BlockDeviceMappings": [
+                {
+                    "DeviceName": "/dev/sda1",
+                    "Ebs": {
+                        "Status": "attached",
+                        "DeleteOnTermination": true,
+                        "VolumeId": "vol-877166c8",
+                        "AttachTime": "2013-07-19T02:42:39.000Z"
+                    }
+                }              
+            ],
+            "Architecture": "x86_64",
+            "StateReason": {
+                "Message": "pending",
+                "Code": "pending"
+            },
+            "RootDeviceName": "/dev/sda1",
+            "VirtualizationType": "hvm",
+            "RootDeviceType": "ebs",
+            "Tags": [
+                {
+                    "Value": "MyInstance",
+                    "Key": "Name"
+                }
+            ],
+            "AmiLaunchIndex": 0
+        }
+    ]
+}
